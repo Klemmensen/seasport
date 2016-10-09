1b06ec90b167d3e96cb08cf15ed2f1892e9b99ae
Jan Klemmensen <jank50@hotmail.com>
2016-10-09 16:05:04 +0200

    Frontend  design

--- seasport/seasport/static/seasport/js/global.js
+++ seasport/seasport/static/seasport/js/global.js
@@ -17,18 +17,3 @@ $( window ).scroll( function()
         $( '.scroll-menu' ).stop( true ).fadeOut();
     }
 });
-/**
- * Created by Jan Klemmensen on 09-10-2016.
- */
-
-// Make sure the info boxes always have the same height
-var highestBox = 0;
-$( '.home-info-box' ).each( function()
-{
-    if( $( this ).height() > highestBox )
-    {
-        highestBox = $( this ).height();
-    }
-});
-// console.log( height );
-$( '.home-info-box' ).height( highestBox );
