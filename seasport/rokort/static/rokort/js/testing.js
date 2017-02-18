9bad12993889dfd3b4b82add4f3633fdd75a5466
Jan Klemmensen <jank50@hotmail.com>
2017-02-18 16:17:15 +0100

    Design

---
+++ seasport/rokort/static/rokort/js/testing.js
@@ -0,0 +1,42 @@
+$( 'body' ).on( 'click', '.btn', function(){
+    $( "body" ).find( "[data-identifier='" + $( this ).attr( 'data-identifier' ) + "']" ).toggleClass( 'btn-active' );
+});
+
+jQuery.fn.extend({
+    toggleText: function (a, b){
+        var that = this;
+            if (that.text() != a && that.text() != b){
+                that.text(a);
+            }
+            else
+            if (that.text() == a){
+                that.text(b);
+            }
+            else
+            if (that.text() == b){
+                that.text(a);
+            }
+        return this;
+    }
+});
+// $( this ).toggleText( $( this ).attr( 'data-first' ), $( this ).attr( 'data-second' ) );
+
+$( function()
+{
+    $('.fixed-content' ).each( function( element )
+    {
+        $( this ).clone().appendTo( $( '.fixed-nav-content' ) );
+    });
+});
+
+$(window).on( 'scroll', function()
+{
+    if ($(window).scrollTop() >= 250 )
+    {
+        $( '.fixed-nav' ).stop().fadeIn();
+    }
+    else
+    {
+        $( '.fixed-nav' ).stop().fadeOut();
+    }
+});
