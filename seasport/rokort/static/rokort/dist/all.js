f59f66a4cab1aa65d80105bf6da2bb83a29b8109
Jan Klemmensen <jank50@hotmail.com>
2017-02-19 00:34:35 +0100

    Design

--- seasport/rokort/static/rokort/dist/all.js
+++ seasport/rokort/static/rokort/dist/all.js
@@ -35,15 +35,29 @@ $(window).on( 'scroll', function()
     if ($(window).scrollTop() >= 205 )
     {
         $( '.fixed-nav' ).stop().show();
+
+        $( 'div.btn-group' ).each( function()
+        {
+            if( !$( this ).parent( 'div' ).parent( 'div' ).parent( 'div' ).hasClass( 'fixed-nav-content' ) )
+            {
+                $( this ).removeClass( 'open' );
+            }
+        });
     }
     else
     {
         $( '.fixed-nav' ).stop().hide();
+        $( 'div.btn-group' ).each( function()
+        {
+            if( $( this ).parent( 'div' ).parent( 'div' ).parent( 'div' ).hasClass( 'fixed-nav-content' ) )
+            {
+                $( this ).removeClass( 'open' );
+            }
+        });
     }
 });
 
-
-// boat-overview-table
+// boat-overview-table list view
 $( '.boat-wrapper' ).each( function( index, element )
 {
     var image   = $( this ).data( 'image' );
@@ -61,9 +75,30 @@ $( '.boat-wrapper' ).each( function( index, element )
 
 });
 
+// Toggle list or image view on boat overview
 $( 'body' ).on( 'click', '.view-toggler', function()
 {
     $( '.' + $( this ).data( 'hide' ) ).hide();
     $( '.' + $( this ).data( 'show' ) ).show();
 });
 
+// Handle multi boat type select list
+$( 'body' ).on( 'change', '.dropdown-menu li input[type=checkbox]', function()
+{
+    var checkedContainer = [];
+    $( this ).parent( 'li' ).parent( 'ul' ).parent( 'div' ).addClass( 'current' );
+
+    $( this ).parent( 'li' ).parent( 'ul' ).find( 'li' ).each( function( index, li )
+    {
+        var checkbox = $( li ).find( 'input[type=checkbox]' );
+
+        if( checkbox.length === 1 && $( checkbox ).is( ':checked' ) )
+            checkedContainer.push( $( checkbox ).val() )
+    });
+    console.log( checkedContainer );
+
+    var content = $( this ).parent( 'li' ).parent( 'ul' ).parent( 'div' ).parent( 'div' ).find( 'div.btn-group' ).clone();
+    $( content ).removeClass( 'open' );
+    $( 'div.btn-group' ).not( '.current' ).replaceWith( content );
+    $( 'div.btn-group' ).removeClass( 'current' );
+});
